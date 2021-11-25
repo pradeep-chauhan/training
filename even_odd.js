d46401e3c652fc8cc9c41b153c6d6201e638154c
Jain-Daksh <daksh44jain@gmail.com>
2021-11-25 17:11:51 +0530

    even and odd in array

---
+++ even_odd.js
@@ -0,0 +1,22 @@
+var a = [1,2,3,4,5,6]
+
+for(i = 0 ; i<a.length ; i++ ){
+    if(a[i] % 2 == 0){
+         print("even number :" , a[i])
+    }
+}
+for(i = 0 ; i<a.length ; i++ ){
+    if(a[i] % 2 !== 0){
+        
+        print("odd number :" , a[i])
+    }
+}
+
+even number : 2
+even number : 4
+even number : 6
+odd number : 1
+odd number : 3
+odd number : 5
+
+
