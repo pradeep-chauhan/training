a64cecd5a6c3f15caceba43b4f79fe47eef51011
Jain-Daksh <daksh44jain@gmail.com>
2021-11-25 14:24:07 +0530

    positive/negative/zero

---
+++ positive_negative-zero.js
@@ -0,0 +1,23 @@
+Positive numbers are greater than zero (eg : 4 5 710 ) 
+Negative numbers are smaller than zero (eg: - 50 -100 )
+
+
+code
+
+
+var n = -5
+
+if (n > 0){
+    print ("positive number :" , n)
+}else if (n < 0){
+    print ("negative number :" , n)
+}else{
+    print ("zero number :" , n)
+}
+
+
+Output ⇒ negative number : -5
+
+
+
+
