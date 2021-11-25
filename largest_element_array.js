e62f94f722a7971f7d8cd0aad969b456f8f95d7d
Jain-Daksh <daksh44jain@gmail.com>
2021-11-25 17:00:33 +0530

    largest element in given array

---
+++ largest_element_array.js
@@ -0,0 +1,13 @@
+var a = [1, 2 ,3 ,4 ,97, 54 , 78 , 101 , 557 , 5, 46 ,7 ,8 ,9 ,10]
+var max = 0 
+
+for (i=0 ; i<a.length ; i++ ){
+    if(a[i] > max){
+        max = a[i]
+    }
+}
+print("largest element in the given array is : " ,  max)
+
+
+
+Output ⇒ largest element in the given array is :  557
