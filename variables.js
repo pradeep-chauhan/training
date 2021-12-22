80a023c8a554b833b4e906a908132d85681555ea
Jain-Daksh <daksh44jain@gmail.com>
2021-12-22 11:34:24 +0530

    variables in js

---
+++ variables.js
@@ -0,0 +1,24 @@
+Data Type
+
+var a = 23 //number
+var b = "init for a string" //string
+var c = 1+3+7-8*5/3 //for maths
+var d = false //for true or false
+var e = 3.14 //for a constant value
+var f = {firstName:"xyz" , lastName:"abc" , age:1}
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)   
+console.log(f.age)
+console.log(f.firstName)
+console.log(f.lastName)
+
+Objects
+
+var g = {fname:"qwe",
+	lname: "rty",
+	nationality: "german"}
