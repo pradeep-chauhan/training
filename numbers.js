9f28a74bebb46f06c438c2da7e86c270aa169d84
Jain-Daksh <daksh44jain@gmail.com>
2021-12-22 21:15:06 +0530

    math in js

---
+++ numbers.js
@@ -0,0 +1,66 @@
+
+numberproperty
+console.log(Math.max(1,5,7));
+
+output==>7
+
+console.log(Math.min(1,5,7));
+
+output ==> 1
+
+numbermethod
+
+n = 2.3121
+num = n.toExponential();
+num2 = n.toFixed(2)
+num3 = n.toPrecision(2)
+num4 = n.toString();
+num5 = n.valueOf()
+console.log("num:" ,num)
+console.log("num2:" ,num2)
+console.log("num3:",num3)
+console.log("num4:",num4)
+console.log("num5:",num5)
+
+output ==> num: 2.3121e+0
+num2: 2.31
+num3: 2.3
+num4: 2.3121
+num5: 2.3121
+
+
+math methods
+
+console.log("Math abs" , Math.abs(-2.3)) //make -ve no to +ve
+console.log("Math acos" , Math.acos(.7))  //number should be b/w -1to1
+console.log("Math asin" , Math.asin(-.3)) //number should be b/w -1to1
+console.log("Math atan" , Math.atan(.3))//number should be b/w -1to1
+console.log("Math atan2" , Math.atan2(-.9 , .5))
+console.log("Math ceil" , Math.ceil(2.3)) //round of to nearest no
+console.log("Math cos" , Math.cos(-.343))//number should be b/w -1to1
+console.log("Math exp" , Math.exp(2,3))
+console.log("Math floor" , Math.floor(-2.3))
+console.log("Math log" , Math.log(2.3))
+console.log("Math pow" , Math.pow(3,4))
+console.log("Math random" , Math.random())
+console.log("Math sin" , Math.sin(.39))//number should be b/w -1to1
+console.log("Math tan" , Math.tan(.75))//number should be b/w -1to1
+console.log("Math sqrt" , Math.sqrt(144))
+
+
+output ==>Math abs 2.3
+Math acos 0.7953988301841436
+Math asin -0.3046926540153975
+Math atan 0.2914567944778671
+Math atan2 -1.0636978224025597
+Math ceil 3
+Math cos 0.9417499633598019
+Math exp 7.38905609893065
+Math floor -3
+Math log 0.832909122935104
+Math pow 81
+Math random 0.2164063276731687
+Math sin 0.3801884151231614
+Math tan 0.9315964599440725
+Math sqrt 12
+
