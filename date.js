dc8f858868191b07dd335e3238799a6047f0e5bd
Jain-Daksh <daksh44jain@gmail.com>
2021-12-22 22:03:32 +0530

    date in js

---
+++ date.js
@@ -0,0 +1,25 @@
+
+pulling date and time value
+
+d = new Date();
+console.log("date",d.getDate())
+console.log("day",d.getDay())
+console.log("year",d.getFullYear())
+console.log("hour",d.getHours())
+console.log("millisec",d.getMilliseconds())
+console.log("sec",d.getSeconds())
+console.log("min",d.getMinutes())
+console.log("time",d.getTime())
+console.log("utc",d.getUTCDate())
+
+
+output ==>date 22
+day 3
+year 2021
+hour 16
+millisec 583
+sec 7
+min 25
+time 1640190307583
+utc 22
+
