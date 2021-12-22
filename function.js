0a5fa708bf992288741c9896c47e9f466b030e5f
Jain-Daksh <daksh44jain@gmail.com>
2021-12-22 22:54:10 +0530

    function

--- function.js
+++ function.js
@@ -1,3 +1,11 @@
-outputting data
+//outputting data
 
+
+alert("hello")
+
+let person = prompt("enter your name", "guest")
+console.log(person)
+
+let deletepost = confirm("do u want to delete ?")
+console.log(deletepost)
 
