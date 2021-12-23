19404df48be07fa549687efc6fa5ba39d2e70767
Jain-Daksh <daksh44jain@gmail.com>
2021-12-23 13:33:13 +0530

    updated function

--- function.js
+++ function.js
@@ -1,11 +1,50 @@
 //outputting data
+/*
+
+alert("hello") //alert
 
 
-alert("hello")
-
+//prompt
 let person = prompt("enter your name", "guest")
 console.log(person)
 
+//confirm
 let deletepost = confirm("do u want to delete ?")
 console.log(deletepost)
 
+
+//global function
+
+//eval method 
+let x = prompt("enter number")
+alert(eval(x))
+alert("sucessful ")        
+
+
+
+//isfinate
+console.log(isFinite(101))
+console.log(isFinite("no"))  
+
+//isnan
+console.log(isNaN(101))
+console.log(isNaN("no"))  
+
+//parseint
+var w="500"
+var y=300
+var z=parseInt(w)+y
+alert(z)
+
+
+//parsefloat
+var q="500.321"
+var e=300
+var r=parseFloat(q)+e
+alert(r)
+*/
+//encode
+
+var  uri = 'https://www.w3schools.com/jsref/jsref_decodeuri.asp'
+var encoded = encodeURI(uri)
+console.log(encoded)
