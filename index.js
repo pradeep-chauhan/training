2344e87c4f1ce81e9cbf01037d65be80f86c3414
5hubham55 <shubhamchoudhary3466@gmail.com>
2022-01-04 04:08:57 +0530

    shubham

---
+++ index.js
@@ -0,0 +1,49 @@
+console.log("javascript file loaded");
+function validation() {
+    fname = document.getElementById("fname").value;
+    lname = document.getElementById("lname").value;
+    email = document.getElementById("email").value;
+   
+        
+    localStorage.setItem("fname",fname)
+    localStorage.setItem("lname",lname)
+    localStorage.setItem("email",email)
+   
+
+   $("#thankyouModal").modal()
+
+}
+
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function myFunction()
+{
+var x = document.getElementById("menu");             
+if(x.style.display == "block")
+    {
+    x.style.display = "none";
+     }
+else{
+    x.style.display = "block";
+  }
+}
+    
